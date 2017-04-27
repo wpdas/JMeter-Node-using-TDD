a2993b9d9acd8507902179df7517b512eb67f121
Wpdas <wpdas@yahoo.com.br>
2017-04-27 04:08:17 -0300

    Up to date

--- modules/jmeter/HTTPMethods.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class HTTPMethods {
-}
-//Get method
-HTTPMethods.GET = 'GET';
-//Post method
-HTTPMethods.POST = 'POST';
-exports.HTTPMethods = HTTPMethods;
